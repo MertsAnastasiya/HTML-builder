577683de1f8aabe9da566e81c7996ebe40d650e1
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-20 22:39:28 +0200

    feat: write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const fileName = path.join(__dirname, 'newFile.txt');
+
+const writeStream = fs.createWriteStream(fileName);
+const {stdin, stdout} = process;
+
+stdout.write('Hello! Enter your text:\n');
+stdin.on('data', text => {
+  writeStream.write(text.toString());
+  if(text.includes('exit')) process.exit();
+});
+
+process.on('exit', () => {
+  console.log('Doi!');
+});
