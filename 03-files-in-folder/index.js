f235407512c82480134eafd49c34d7073b547acf
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-21 20:25:14 +0200

    feat: read info about files

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const fileName = path.join(__dirname, 'secret-folder');
+const { stdout } = process;
+stdout.write(fileName);
+
+
+const readDirectory = (fileName) => {
+  fs.readdir(fileName, (error, files) => {
+    if(error) throw error; 
+    files.map(item => {
+      let fullPath = path.join(fileName, item);
+      getResult(fullPath);
+    });
+  });
+};
+
+const getResult = (fullPath) => {
+  if(fs.lstatSync(fullPath).isFile()) {
+    let stats = fs.statSync(fullPath);
+    const size = stats.size/1000 + 'kb';
+    const extension = path.extname(fullPath);
+    const name = path.basename(fullPath).replace(extension, '');
+    console.log(`${name} - ${extension} - ${size}`);
+  } else {
+    readDirectory(fullPath);
+  }
+};
+
+console.log('\nFiles: ');
+readDirectory(fileName);
