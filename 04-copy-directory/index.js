96a08e7b0db8f98a6bba29e88d1e10e1dbf01d12
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-22 00:47:51 +0200

    feat: copy files from folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('node:fs');
+const path = require('node:path');
 
+const copyFrom= path.join(__dirname, 'files');
+const copyTo= path.join(__dirname, 'files-copy');
+
+fs.readdir(copyFrom, (error, files) => {
+  if(error) throw error; 
+  fs.access(copyTo, (error) => {
+    if (error) {
+      fs.mkdir(copyTo, (error) => {
+        if (error)  throw error;
+        console.log('Create folder');
+        copyFiles(files);
+      });}
+    else {
+      copyFiles(files);
+    }
+  });
+});
+
+const copyFiles = (files) => {
+  files.forEach(file => {
+    const readStream = fs.createReadStream(path.join(copyFrom, file));
+    const writeStream = fs.createWriteStream(path.join(copyTo,file));
+    readStream.pipe(writeStream);
+  });
+};
