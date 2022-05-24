95932942e9fc8214792e9d40f3bec5714535091c
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-24 22:00:29 +0200

    fix: fix reflection of changes in the folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,22 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const fsPromises = require('fs').promises;
 
 const copyFrom= path.join(__dirname, 'files');
 const copyTo= path.join(__dirname, 'files-copy');
 
 fs.readdir(copyFrom, (error, files) => {
   if(error) throw error; 
-  fs.access(copyTo, (error) => {
-    if (error) {
-      fs.mkdir(copyTo, (error) => {
-        if (error)  throw error;
-        console.log('Create folder');
-        copyFiles(files);
-      });}
-    else {
+  fs.access(copyTo, async () => {
+    await fsPromises.rm(copyTo, { recursive: true, force: true });
+    fs.mkdir(copyTo, (error) => {
+      if (error)  throw error;
+      console.log('Create folder');
       copyFiles(files);
-    }
+    });
   });
 });
+
 
 const copyFiles = (files) => {
   files.forEach(file => {
